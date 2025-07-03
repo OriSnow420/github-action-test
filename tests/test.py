5330ad0b3f094a60a8a28457e1d0f65ac17bd548
OriSnow420 <1315376634@qq.com>
2025-07-03 09:00:18 +0800

    fix: wrong code position

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import unittest
+from src.utils import factorial
+
+class TestFactorial(unittest.TestCase):
+
+    def test_zero_factorial_should_be_one(self):
+        self.assertEqual(factorial(0), 1)
+
+    def test_a_default_number_factorial(self):
+        number = 6 # 6! = 24         # Assign
+        result = factorial(number)   # Act
+        self.assertEqual(result, 720) # Assert
